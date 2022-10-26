e6f00ad00d94e2299cdba9dc318e7c1ec05da913
Anu Katti <99070971+AnuKatti@users.noreply.github.com>
2022-10-26 16:45:03 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Product from './productDetails';
+import JSON from './db.json';
 
-const App = () => {
-    return(
-        <>
-            <Header/>
-            <h1 style ={{textAlign:'center'}} >Hi from React</h1>
-            <h2 style ={{textAlign:'center'}}> Test Component</h2>
-            <Footer year="2022" month="Oct"/>
-        </>
-    )
+class App extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            productData:JSON,
+            filteredData:JSON
+        }
+    }
+    filteredData = (keyword) => {
+        let output=this.state.productData.filter((data) => {
+            return data.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1
+        })
+    
+        this.setState({filteredData:output})
+
+        let outputbrand=this.state.productData.filter((data) => {
+            return data.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1
+        })
+    
+        this.setState({filteredData:outputbrand})
+
+        // let outputcost=this.state.productData.filter((data) => {
+        //     return (data.cost <= 200)
+        // })
+    
+        // this.setState({filteredData:outputcost})
+    }
+    render(){
+        return(
+            <>
+                <Header userInput={(data) =>{this.filteredData(data)} }/>
+                <Product prodData={this.state.filteredData}/>
+                <Footer year="2022" month="Oct"/>
+            </>
+        )
+    }
 }
+    
 
 export default App;
