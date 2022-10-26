511e7f94c2acbcaeb555ab6453df9964034cd35e
Anu Katti <99070971+AnuKatti@users.noreply.github.com>
2022-10-26 16:45:49 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import './Header.css';
+
 class Header extends Component {
 
     constructor(){
-        super();
+        super()
         this.state = {
             title:"React Application",
-            keyword:"User Text Here"
+            keyword:"User Text Here",
         }
     }
 
     handleChange = (event) => {
-        this.setState({keyword:event.target.value})
-    }
-
-    noChange = (event) => {
-        this.setState(keyword)
+        this.setState({keyword:event.target.value? event.target.value : 'User Text Here'})
+        this.props.userInput(event.target.value)
     }
 
     render(){
@@ -24,7 +22,8 @@ class Header extends Component {
                 <header>
                     <div className = "logo" >{this.state.title}</div>
                     <input onChange = {this.handleChange}/>
-                    <div style = {{fontSize:'25px',color:'white'}}> {{onChange ? 'handleChange' : 'noChange'}} </div>
+                    <input onChange = {this.handleChange}/>
+                    <div style = {{fontSize:'25px',color:'white'}}> {this.state.keyword} </div>
                 </header>
                 <hr/>
             </Fragment>
